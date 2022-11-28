cd2f95e8a9e708ae750e2f5e3474cc36a00181aa
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-28 17:20:53 +0100

    final

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Program{
-    class Replace{
-        public static void Main(){
-                
+﻿namespace Program
+{
+    class Replace
+    {
+        public static void Main()
+        {
+            static void ReplaceWords(string s, char c)
+            {
+                string[] MyString =  s.Split(c);
+                Console.WriteLine(MyString[1] + c + MyString[0]);
+            }
+            ReplaceWords("trolling.master", '.');
+            ReplaceWords("abc_xyz", '_');
         }
     }
 }
